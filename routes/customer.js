c95ebc1a9e3215e287a801fbd6a0fcd9364e7820
hoang1101 <lhhoang1101@gmail.com>
2022-12-25 21:07:29 +0700

    customer

--- routes/customer.js
+++ routes/customer.js
@@ -1,39 +1,57 @@
-const { createCustomer, updateCustomer, seeStatus, findOrder, updateCustomerSender, getAllCommodities, createReport, editReport, deleteReport, getAllReport } = require("../controllers/customer");
-const { createOrder, getAllOrder, editOrderF, getAllOrderF, deleteOrder, GetOrderId } = require("../controllers/order");
+const {
+  createCustomer,
+  updateCustomer,
+  seeStatus,
+  findOrder,
+  updateCustomerSender,
+  getAllCommodities,
+  createReport,
+  editReport,
+  deleteReport,
+  getAllReport,
+} = require("../controllers/customer");
+const {
+  createOrder,
+  getAllOrder,
+  editOrderF,
+  getAllOrderF,
+  deleteOrder,
+  GetOrderId,
+} = require("../controllers/order");
 
 const router = require("express").Router();
-const {verifyToken} = require("../middleware/middleware")
+const { verifyToken } = require("../middleware/middleware");
 //tao thong tin tai khoan 8
 // router.post('/create',verifyToken,createCustomer)/
 // cap nhat thong tin Customer 9
-router.put('/update/:id',updateCustomer)
+router.put("/update/:id", updateCustomer);
 //xem trang thai don hang 6
-router.get('/getstatus/:id',seeStatus)
+router.get("/getstatus/:id", seeStatus);
 // tim kiem don hang 4
-router.get('/getorder/:id',findOrder)
+router.get("/getorder/:id", findOrder);
 //tao don hang moi 2
-router.post('/createorder',verifyToken,createOrder)
+router.post("/createorder", verifyToken, createOrder);
 // xem danh sach cac don hang chua nhan
-router.get('/getall',verifyToken,getAllOrder)
-// xem danh sach cac don hang chua duoc giao 
-router.get('/getallf',verifyToken,getAllOrderF)
+router.get("/getall", verifyToken, getAllOrder);
+// xem danh sach cac don hang chua duoc giao
+router.get("/getallf", verifyToken, getAllOrderF);
 // xoa mot don hang 5
-router.delete('/delete/:id',deleteOrder)
+router.delete("/delete/:id", deleteOrder);
 // chinh sua don hang chua duoc giao 3
-router.put('/updateOrder/:id',editOrderF)
+router.put("/updateOrder/:id", editOrderF);
 // add dia chi lay jhang va sdt
-router.put('/updateOrderSender/:id',updateCustomerSender);
+router.put("/updateOrderSender/:id", updateCustomerSender);
 // lay ra danh sach cac loai hang hoa
-router.get('/getallcommodities',getAllCommodities);
-// tao mot report 
-router.post('/createreport',verifyToken,createReport);
+router.get("/getallcommodities", getAllCommodities);
+// tao mot report
+router.post("/createreport", verifyToken, createReport);
 // update report
-router.put('/updatereport/:id',verifyToken,editReport);
-// delete report 
-router.delete('/deletereport/:id',verifyToken,deleteReport);
+router.put("/updatereport/:id", verifyToken, editReport);
+// delete report
+router.delete("/deletereport/:id", verifyToken, deleteReport);
 // lay danh sach cac report
-router.get('/getallreport',verifyToken,getAllReport);
-// lay ra mot don hang theo id 
-router.get("/order/:id",GetOrderId);
+router.get("/getallreport", verifyToken, getAllReport);
+// lay ra mot don hang theo id
+router.get("/order/:id", GetOrderId);
 
 module.exports = router;
